ec9a34dc2bd2433dd93300ae4ee39a2270125727
강승민 <rkdtmdals999@naver.com>
2023-07-05 11:16:20 +0900

    Add variable.dart

---
+++ Variable/variable.dart
@@ -0,0 +1,12 @@
+// 타입 안전성이 높은 Dart 코드일지라도,
+// var를 사용하여 명시적으로 타입을 지정하지 않고 변수를 선언할 수 있습니다.
+// 타입 추론 덕분에 이런 변수들의 타입은 초기 값을 통해 결정됩니다.
+
+var name = "승민";
+var year = 2023;
+var antennaDiameter = 3.7;
+var flybyObjects = ["Jupyter", "Saturn", "Uranus", "Neptuen"];
+var image = {
+  'tags': ['saturn'],
+  'url': "//path/to/saturn.jpg"
+};
